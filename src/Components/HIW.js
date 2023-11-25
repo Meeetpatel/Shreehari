9ecc8280ebd56e7e408296f4cf6a8d668b6bf199
Meet Patel <meetpatel4567.mp@gmail.com>
2023-11-25 08:09:52 -0500

    How it Works works

--- src/Components/HIW.js
+++ src/Components/HIW.js
@@ -1,11 +1,20 @@
 import React from "react";
-import pic5 from "../pic-3.jpg";
+import bg1 from "../bg1.jpeg";
 import "./styles.css";
+import useEmblaCarousel from "embla-carousel-react";
+import before from "../Before.jpg";
+import after from "../After.jpg";
+import Autoplay from "embla-carousel-autoplay";
+import bedroom from "../bedroom.jpg";
+import bg3 from "../bg3.jpg";
+import bg4 from "../bg4.jpg";
+import bg5 from "../bg5.jpg";
 const HIW = () => {
   const containerStyles = {
-    height: "70vh", // Adjusted for full viewport height
-    width: "90vh",
-    background: `url(${pic5})`, // Replace with your image URL
+    height: "100vh", // Adjusted for full viewport height
+    width: "100%",
+    border: " 2px solid #F0F8FF ",
+    background: `url(${bg1})`, // Replace with your image URL
     backgroundSize: "cover",
     backgroundPosition: "center",
     display: "flex",
@@ -13,6 +22,8 @@ const HIW = () => {
     alignItems: "center",
     color: "black", // Adjust the text color
   };
+
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
   return (
     <>
@@ -22,8 +33,98 @@ const HIW = () => {
           <h3 className="title2">Easiest way to Build your Home</h3>
         </div>
       </div>
+      <div className="process1">
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container">
+            <div className="embla__slide">
+              <img
+                src={before}
+                border=" 2px solid #F0F8FF "
+                width="50%"
+                height="90%"
+                alt="before"
+              />
+            </div>
+            <div className="embla__slide">
+              <img
+                src={after}
+                border=" 2px solid #F0F8FF "
+                width="50%"
+                height="90%"
+                alt="after"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h1 className="process-title">STEP 1</h1>
+          <h1 className="process-description">
+            Contact Us to schedule a Meeting With us in person.
+          </h1>
+        </div>
+      </div>
       <div className="process">
-        <h4 className="process-title">STEP 1</h4>
+        <div>
+          <h1 className="process-title">STEP 2</h1>
+          <h1 className="process-description">
+            Tell us all about your dream home. We’ll get to work on bringing it
+            to life.
+          </h1>
+        </div>
+        <img
+          width="40%"
+          border=" 2px solid #F0F8FF "
+          height="50%"
+          src={bedroom}
+          alt="img"
+        />
+      </div>
+      <div className="process">
+        <img
+          width="40%"
+          border=" 2px solid #F0F8FF "
+          height="50%"
+          src={bg3}
+          alt="img"
+        />
+        <div>
+          <h1 className="process-title">STEP 3</h1>
+          <h1 className="process-description">
+            Get an estimated Quote for your Residential or Work Space.
+          </h1>
+        </div>
+      </div>
+      <div className="process">
+        <div>
+          <h1 className="process-title">STEP 4</h1>
+          <h1 className="process-description">
+            We will blow your mind with inspo and ideas for transforming your
+            space.
+          </h1>
+        </div>
+        <img
+          width="40%"
+          border=" 2px solid #F0F8FF "
+          height="50%"
+          src={bg4}
+          alt="img"
+        />
+      </div>
+      <div className="process">
+        <img
+          width="40%"
+          border=" 2px solid #F0F8FF "
+          height="50%"
+          src={bg5}
+          alt="img"
+        />
+        <div>
+          <h1 className="process-title">STEP 5</h1>
+          <h1 className="process-description">
+            Tell us which design ideas you vibe with, and we’ll add all the
+            beautiful finishing touches.
+          </h1>
+        </div>
       </div>
     </>
   );
